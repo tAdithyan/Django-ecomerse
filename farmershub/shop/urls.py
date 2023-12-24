6a5563d9eb24992d01aec55a2e425fc87551f314
tAdithyan <adithyanttech@gmail.com>
2023-12-24 09:50:09 +0530

    login  page backend created

--- farmershub/shop/urls.py
+++ farmershub/shop/urls.py
@@ -5,7 +5,11 @@ from .import views
 urlpatterns = [
    
     path('',views.shop,name='shop'),
-    path('login/',views.login,name='login')
+    path('login/',views.log_in,name='login'),
+    path('signup/',views.sign_up,name="signup"),
+    path('logout/',views.log_out,name='logout')
+
+
     
   
 ]
--- farmershub/shop/views.py
+++ farmershub/shop/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from.models import *
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 
 # Create your views here.
 def shop(request):
   return render(request,'shop/shop.html')
-def login(request):
-  return render(request,'login_signup/login/login.html')
+def log_in(request):
+ if request.method=="POST":
+
+
+  email=request.POST.get('email')
+
+  password=request.POST.get('password')
+  user =authenticate(request, username=email, password=password)
+  
+  
+  if user is not None:
+     login(request,user)
+  else:
+     pass
+  return redirect("shop")
+ 
+
+ return render(request,'login_signup/login/login.html')
+def sign_up(request):
+  if request.method=="POST":
+
+   fname=request.POST.get('name')
+   email=request.POST.get('email')
+
+   password=request.POST.get('password')
+
+   user = User.objects.create_user(first_name=fname,username=email,email=email, password=password)
+ 
+   user.save()
+   return redirect(log_in)
+
+  return render(request,'login_signup/signup/signup.html')
+def log_out(request):
+    logout(request)
+    return redirect("shop")
