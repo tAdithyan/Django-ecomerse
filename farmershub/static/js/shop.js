b80f686a1da4a180e2cb451eee1da267f63c67d3
tAdithyan <adithyanttech@gmail.com>
2023-12-26 14:45:43 +0530

    newproduct section added

---
+++ farmershub/static/js/shop.js
@@ -0,0 +1,22 @@
+
+
+
+function increaseCount(a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt(input.value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    input.value = value;
+  }
+  
+  function decreaseCount(a, b) {
+    var input = b.nextElementSibling;
+    var value = parseInt(input.value, 10);
+    if (value > 0) {
+      value = isNaN(value) ? 0 : value;
+      value--;
+      input.value = value;
+    }
+  }
+
+
