69f0bed1559f45046c83110a18fbaf4c4f6cd383
tAdithyan <adithyanttech@gmail.com>
2023-12-29 22:06:30 +0530

    category section backend created

--- farmershub/shop/models.py
+++ farmershub/shop/models.py
@@ -2,18 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Categories(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100,null=True,unique=True)
+    categoryname=models.CharField(max_length=100,null=True,unique=True)
+    image=models.ImageField(upload_to='category', null=True)
+
+
+
     def __str__(self) -> str:
      return self.title
     
 
 class banner_area(models.Model):
-   image=models.ImageField(upload_to='banner')
-   title=models.CharField(max_length=100,default="")
-   deal=models.CharField(max_length=50,default="")
-   sale=models.IntegerField()
-   discount=models.IntegerField()
-   link=models.CharField(max_length=100,default="")
+   image=models.ImageField(upload_to='banner', null=True)
+   title=models.CharField(max_length=100,default="" ,null=True)
+   deal=models.CharField(max_length=50,default="",null=True)
+   sale=models.IntegerField(null=True)
+   discount=models.IntegerField(null=True)
+   link=models.CharField(max_length=100,default="",null=True,blank=True)
    def __str__(self) -> str:
      return self.title
     
