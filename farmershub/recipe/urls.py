997a88383062233e9b526af02aad49d23fe87a68
tAdithyan <adithyanttech@gmail.com>
2024-01-27 09:43:54 +0530

    recipe iteam edited

--- farmershub/recipe/urls.py
+++ farmershub/recipe/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
    
     path('',views.recipe,name='recipe'),
-    path('content/<str:id>',views.content,name="content"),
+    path('content/<int:id>',views.content,name="content"),
     path('search/',views.search,name='search'),
     path('addrecipe/',views.addrecipe,name='addrecipe'),
     path('editrecipes/<int:id>',views.editrecipes,name='editrecipes'),
