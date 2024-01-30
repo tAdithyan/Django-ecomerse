890f6dca2c84462b154555f9b118ae8488bd281c
“tech-karippilly” <vishnukarippilly@gmail.com>
2024-01-30 18:30:08 +0530

    javascript addded

--- farmershub/static/js/shop2.js
+++ farmershub/static/js/shop2.js
@@ -1,9 +1,9 @@
 
 
-
 let menu = document.querySelector("#menu-bar");
 let navbar = document.querySelector(".navbar");
 let header = document.querySelector(".header-2");
+
 menu.addEventListener("click", () => {
   menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
@@ -19,3 +19,27 @@ window.onscroll = () => {
   }
 };
 
+function addToCart(id, price,name) {
+  console.log('working');
+  console.log(" Product Id :", id);
+  console.log(' Price ', price);
+
+  let quantity = document.getElementById('quantity').value
+  console.log('quantity:', quantity);
+
+  var cartData = JSON.parse(localStorage.getItem('cart')) || [];
+
+  let Item = {
+    UserId: "",
+    productId: id,
+    productPrice: price,
+    quantity,
+    productName:name
+  }
+
+  cartData.push(Item)
+
+  localStorage.setItem('cart', JSON.stringify(cartData));
+  var retrievedCartData = JSON.parse(localStorage.getItem("cart"));
+  console.log(retrievedCartData);
+} 
