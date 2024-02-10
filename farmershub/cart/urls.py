e7df6b534d28392cc0abbb16998eea8676b7e789
tAdithyan <adithyanttech@gmail.com>
2024-02-10 09:27:17 +0530

    cartsection created

--- farmershub/cart/urls.py
+++ farmershub/cart/urls.py
@@ -3,9 +3,8 @@ from django.urls import path,include
 from .  import views
 urlpatterns = [
     path('cart/',views.cart,name='cart'),
-    path('add/',views.cart_add,name='cart_add'),
-    path('delete/',views.cart_delete,name='cart_delete'),
-    path('update/',views.cart_update,name='cart_update'),
+  
+    path('orderconfir/',views.orderconfir,name='orderconfir')
 
   
 ]
